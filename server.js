46483ca571848a0be01941367a8daa1118f51471
jacwida-hub <jacwida@gmail.com>
2021-02-20 16:54:46 +0300

    created server js and add express and dotenv

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+//load env vars
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  )
+);
